02799efb699fae5c463f3a0449f877d01f9a7afd
myceey69 <126042972+myceey69@users.noreply.github.com>
2025-07-12 01:24:08 -0700

    Update user_prompts.py

--- filt_gen/user_prompts.py
+++ filt_gen/user_prompts.py
@@ -46,18 +46,6 @@ import utils.data_products as data_products
 import utils.filters as filters
 import utils.utilities as utilities
 
-from colorama import init, Fore, Style
-init(autoreset=True)
-
-RED = "\033[91m"
-GREEN = "\033[92m"
-YELLOW = "\033[93m"
-BLUE = "\033[94m"
-MAGENTA = "\033[95m"
-CYAN = "\033[96m"
-BOLD = "\033[1m"
-RESET = "\033[0m"
-
 
 def validate_input(user_input, max_ok_val):
     try:
